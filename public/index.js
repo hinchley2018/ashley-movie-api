7c6c41cb828f15e55bfb940903be8efa261ce4c9
theashleyromick <ashley.romick@gmail.com>
2024-08-15 14:24:27 -0400

    install morgan

--- public/index.js
+++ public/index.js
@@ -45,17 +45,7 @@ let topMovies = [
 },
 ];
 
-app.get('/', (req, res) => {
-  res.send('Welcome to myFlix!');
-});
-
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
-
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
+app.use(express.static('public'));
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
