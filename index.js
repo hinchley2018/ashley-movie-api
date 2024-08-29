4f880f42bc7a7faf877ea18b4764a0bfe9e0e0af
theashleyromick <ashley.romick@gmail.com>
2024-08-29 14:12:14 -0400

    Add password hashing.

--- index.js
+++ index.js
@@ -186,18 +186,31 @@ app.post('/movies', passport.authenticate('jwt', { session: false }), async (req
 
 // POST new Users
 app.post('/users', async (req, res) => {
-  try {
-    const newUser = new User({
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
     });
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(500).send('Error: ' + err.message);
-  }
 });
 
 // POST users favorite movies
--- passport.js
+++ passport.js
@@ -37,15 +37,35 @@ passport.use(
 );
 
 
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-  }, async (jwtPayload, callback) => {
-    return await Users.findById(jwtPayload._id)
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password',
+    },
+    async (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      await Users.findOne({ Username: username })
       .then((user) => {
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, {
+            message: 'Incorrect username or password.',
+          });
+        }
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect password.' });
+        }
+        console.log('finished');
         return callback(null, user);
       })
       .catch((error) => {
-        return callback(error)
-      });
-  }));
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+      })
+    }
+  )
+);
