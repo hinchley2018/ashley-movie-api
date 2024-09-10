97d59390dfeaa190bead29ebcd4ab54f875fa478
theashleyromick <ashley.romick@gmail.com>
2024-09-10 14:19:25 -0400

    change auth.js to ensure authorization is working.

--- auth.js
+++ auth.js
@@ -15,22 +15,25 @@ let generateJWTToken = (user) => {
 
 
 /* POST login. */
-module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
-        return res.status(400).json({
-          message: 'Something is not right',
-          user: user
-        });
-      }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
-          res.send(error);
-        }
-        let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
+app.post('/login', (req, res) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something went wrong',
+        user: user
       });
-    })(req, res);
-  });
-}
+    }
+
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        res.send(err);
+      }
+
+      // Generate JWT Token
+      const token = jwt.sign(user.toJSON(), process.env.SECRET_KEY, {
+        expiresIn: '7d',
+      });
+      return res.json({ user, token });
+    });
+  })(req, res);
+});
