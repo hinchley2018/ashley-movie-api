4eda6625f9a4e6cb6f43f7ec5165820006413ab4
theashleyromick <ashley.romick@gmail.com>
2024-09-16 09:40:15 -0400

    Fix userRoutes.js imports.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { check, validationResult } = require('express-validator');
-const movie = require('../models/userModel.js');
+const User = require('../models/userModel.js');
+const Movie = require('../models/movieModel.js');
 
 // POST new Users
 router.post('/',
